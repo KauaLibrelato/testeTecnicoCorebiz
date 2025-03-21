2d20f94257621ef34aa57b0d8b3cb1ab126f12f6
KauaLibrelato <kaualibrelatodacosta@gmail.com>
2025-03-21 17:33:36 -0300

    feat: create axios instance

--- src/screens/Screen1/Screen1.tsx
+++ src/screens/Screen1/Screen1.tsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 
+import { api } from "../../services/api";
 import * as S from "./Screen1Styles";
 
 export function Screen1() {
+    async function teste() {
+        try {
+            await api.get("/search/photos?page=1&query=office").then((response) => {
+                // console.log(response.data?.results);
+            });
+        } catch (error) {
+            console.log("Error:", error);
+        }
+    }
+
+    useEffect(() => {
+        teste();
+    }, []);
     return (
         <S.Container>
             <S.Title>Screen1</S.Title>
